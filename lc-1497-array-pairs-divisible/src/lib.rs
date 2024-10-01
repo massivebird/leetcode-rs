30812e587a4de9ebe78237b3c40dc847714b3919
massivebird <33991373+massivebird@users.noreply.github.com>
2024-10-01 12:04:08 -0400

    init: 1497

---
+++ lc-1497-array-pairs-divisible/src/lib.rs
@@ -0,0 +1,17 @@
+struct Solution;
+
+impl Solution {
+    pub fn can_arrange(arr: Vec<i32>, k: i32) -> bool {
+        todo!();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert!(Solution::can_arrange(vec![1,2,3,4,5,10,6,7,8,9], 5))
+    }
+}
