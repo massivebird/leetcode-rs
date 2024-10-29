9557563950aeee4d58ac8ae51893ce2ee2e3bc15
massivebird <33991373+massivebird@users.noreply.github.com>
2024-10-29 11:15:20 -0400

    init: 1545

---
+++ lc-1545-kth-bit-in-kth-binary-string/src/lib.rs
@@ -0,0 +1,18 @@
+struct Solution {}
+
+#[allow(dead_code)]
+impl Solution {
+    pub fn find_kth_bit(n: i32, k: i32) -> char {
+        todo!();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(Solution::find_kth_bit(3, 1), '0');
+    }
+}
