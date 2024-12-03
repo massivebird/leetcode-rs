aebad93384ac0822cf3835890ab54e8418297a11
massivebird <33991373+massivebird@users.noreply.github.com>
2024-12-03 11:36:23 -0500

    wip: timing out on large input string

--- lc-2109-adding-spaces/src/lib.rs
+++ lc-2109-adding-spaces/src/lib.rs
@@ -5,7 +5,21 @@ struct Solution {}
 impl Solution {
     #[allow(clippy::needless_pass_by_value)]
     pub fn add_spaces(s: String, spaces: Vec<i32>) -> String {
-        todo!();
+        let mut result: Vec<char> = Vec::new();
+
+        if spaces.contains(&0) {
+            result.push(' ');
+        }
+
+        for (idx, c) in s.char_indices() {
+            result.push(c);
+
+            if spaces.contains(&((idx + 1) as i32)) {
+                result.push(' ');
+            }
+        }
+
+        result.iter().collect::<String>()
     }
 }
 
@@ -25,7 +39,15 @@ mod tests {
     fn case_2() {
         assert_eq!(
             "i code in py thon".to_string(),
-            Solution::add_spaces("icodeinpython".to_string(), vec![1,5,7,9])
+            Solution::add_spaces("icodeinpython".to_string(), vec![1, 5, 7, 9])
+        );
+    }
+
+    #[test]
+    fn case_3() {
+        assert_eq!(
+            " s p a c i n g".to_string(),
+            Solution::add_spaces("spacing".to_string(), vec![0, 1, 2, 3, 4, 5, 6])
         );
     }
 }
