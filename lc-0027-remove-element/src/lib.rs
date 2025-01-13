650944a97c9e07c340ceb1805805d86c961d5a0b
massivebird <33991373+massivebird@users.noreply.github.com>
2025-01-13 14:28:16 -0500

    feat(0027): complete

--- lc-0027-remove-element/src/lib.rs
+++ lc-0027-remove-element/src/lib.rs
@@ -2,7 +2,20 @@ struct Solution;
 
 impl Solution {
     pub fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
-        todo!();
+        let mut new_nums: Vec<i32> = Vec::new();
+
+        let mut num_not_matching = 0;
+
+        for e in nums.iter() {
+            if *e != val {
+                new_nums.push(*e);
+                num_not_matching += 1;
+            }
+        }
+
+        *nums = new_nums;
+
+        num_not_matching
     }
 }
 
@@ -14,5 +27,6 @@ mod tests {
     fn case_1() {
         let mut a = vec![3, 2, 2, 3];
         assert_eq!(Solution::remove_element(&mut a, 3), 2);
+        assert_eq!(a, vec![2, 2]);
     }
 }
