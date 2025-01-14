71f1ac94df4e23249dd14cdefb9ba895c476f0b6
massivebird <33991373+massivebird@users.noreply.github.com>
2025-01-14 12:01:25 -0500

    changed(0169): optimization

--- lc-0169-majority-element/src/lib.rs
+++ lc-0169-majority-element/src/lib.rs
@@ -3,30 +3,22 @@ use std::collections::HashMap;
 struct Solution;
 
 impl Solution {
+    // KEY: assume some majority element always exists.
+    // We can quit as soon as a value's freq exceeds floor(n/2).
     pub fn majority_element(nums: Vec<i32>) -> i32 {
         let mut freqs: HashMap<i32, u32> = HashMap::new();
 
-        freqs.insert(nums[0], 1);
-        let mut majority_val: i32 = nums[0];
-        let mut majority_freq: u32 = 1;
+        let goal = u32::try_from(nums.len() / 2).unwrap();
 
-        for e in nums.into_iter().skip(1) {
-            freqs.entry(e).and_modify(|f| *f += 1).or_insert(1);
+        for n in nums {
+            freqs.entry(n).and_modify(|f| *f += 1).or_insert(1);
 
-            let this_freq = freqs.get(&e).unwrap();
-
-            if e == majority_val {
-                majority_freq += 1;
-                continue;
-            }
-
-            if *this_freq > majority_freq {
-                majority_val = e;
-                majority_freq = *this_freq;
+            if *freqs.get(&n).unwrap() > goal {
+                return n;
             }
         }
 
-        majority_val
+        unreachable!();
     }
 }
 
@@ -36,11 +28,11 @@ mod tests {
 
     #[test]
     fn case_1() {
-        assert_eq!(Solution::majority_element(vec![3,2,3]), 3);
+        assert_eq!(Solution::majority_element(vec![3, 2, 3]), 3);
     }
 
     #[test]
     fn case_2() {
-        assert_eq!(Solution::majority_element(vec![6,5,5]), 5);
+        assert_eq!(Solution::majority_element(vec![6, 5, 5]), 5);
     }
 }
