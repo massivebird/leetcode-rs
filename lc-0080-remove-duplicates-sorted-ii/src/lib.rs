c63cf950d074452bc7c525aeb1cf6702eba820f2
massivebird <33991373+massivebird@users.noreply.github.com>
2025-02-02 22:23:08 -0500

    feat(0080): complete

--- lc-0080-remove-duplicates-sorted-ii/src/lib.rs
+++ lc-0080-remove-duplicates-sorted-ii/src/lib.rs
@@ -4,38 +4,44 @@ impl Solution {
     // MUST perform operations in place without another array
     pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
         if nums.len() <= 2 {
-            return 2;
+            return nums.len() as i32;
         }
 
-        let mut write_idx = 1;
-        let mut latest_uniq: i32 = nums[0];
-        let mut latest_uniq_capped: bool = false;
+        let mut write_idx = 2;
+        let mut latest_uniq: i32 = nums[1];
+        let mut latest_uniq_capped: bool = nums[0] == nums[1];
 
-        for read_idx in 1..nums.len() {
+        for read_idx in 2..nums.len() {
             if write_idx >= nums.len() {
                 break;
             }
 
             let this = nums[read_idx];
 
+            // We have enough of this number.
+            if this == latest_uniq && latest_uniq_capped {
+                continue;
+            }
+
+            // Second of the latest number. No more after this!
+            if this == latest_uniq && !latest_uniq_capped {
+                latest_uniq_capped = true;
+                nums[write_idx] = this;
+                write_idx += 1;
+                continue;
+            }
+
+            // New number!
             if this != latest_uniq {
                 latest_uniq = this;
                 latest_uniq_capped = false;
                 nums[write_idx] = this;
                 write_idx += 1;
-            }
-
-            if latest_uniq_capped {
                 continue;
             }
-
-            nums[write_idx] = this;
-            latest_uniq_capped = true;
-            write_idx += 1;
         }
 
-        // dbg!(nums);
-        write_idx as i32 - 1
+        write_idx as i32
     }
 }
 
