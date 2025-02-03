d1f005bb42b73131010d6dfb685429a755469ed6
massivebird <33991373+massivebird@users.noreply.github.com>
2025-02-02 22:43:11 -0500

    feat(0080): hella optimized

--- lc-0080-remove-duplicates-sorted-ii/src/lib.rs
+++ lc-0080-remove-duplicates-sorted-ii/src/lib.rs
@@ -8,39 +8,16 @@ impl Solution {
         }
 
         let mut write_idx = 2;
-        let mut latest_uniq: i32 = nums[1];
-        let mut latest_uniq_capped: bool = nums[0] == nums[1];
 
         for read_idx in 2..nums.len() {
-            if write_idx >= nums.len() {
-                break;
-            }
-
-            let this = nums[read_idx];
-
-            // We have enough of this number.
-            if this == latest_uniq && latest_uniq_capped {
-                continue;
-            }
-
-            // Second of the latest number. No more after this!
-            if this == latest_uniq && !latest_uniq_capped {
-                latest_uniq_capped = true;
-                nums[write_idx] = this;
-                write_idx += 1;
-                continue;
-            }
-
-            // New number!
-            if this != latest_uniq {
-                latest_uniq = this;
-                latest_uniq_capped = false;
-                nums[write_idx] = this;
+            if nums[read_idx] != nums[write_idx - 2] {
+                nums[write_idx] = nums[read_idx];
                 write_idx += 1;
                 continue;
             }
         }
 
+        // dbg!(&nums);
         write_idx as i32
     }
 }
