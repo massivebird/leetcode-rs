cef6fdcd9afbdd225b250e5b53cbbecc839511de
massivebird <33991373+massivebird@users.noreply.github.com>
2025-02-12 13:06:01 -0500

    wip: even displacements not working

--- lc-0189-rotate-array/src/lib.rs
+++ lc-0189-rotate-array/src/lib.rs
@@ -8,9 +8,15 @@ impl Solution {
         // Each value displaces some other value.
         // Let's move the first one, pick up whatever it
         // displaces, then move that one.
+        // BUUUUUTTTTT even displacement displaces only a SUBSET
 
         for _ in 0..nums.len() {
+            let new_idx = (hand_og_idx + k as usize) % nums.len();
 
+            // Swap displaced value with hand
+            std::mem::swap(&mut nums[new_idx], &mut hand);
+            hand_og_idx = new_idx;
+            println!("{nums:?}")
         }
     }
 }
@@ -21,8 +27,15 @@ mod tests {
 
     #[test]
     fn case_1() {
-        let mut arr = vec![1,2,3,4,5,6,7];
+        let mut arr = vec![1, 2, 3, 4, 5, 6, 7];
         Solution::rotate(&mut arr, 3);
-        assert_eq!(arr, vec![5,6,7,1,2,3,4]);
+        assert_eq!(arr, vec![5, 6, 7, 1, 2, 3, 4]);
+    }
+
+    #[test]
+    fn case_2() {
+        let mut arr = vec![-1,-100,3,99];
+        Solution::rotate(&mut arr, 2);
+        assert_eq!(arr, vec![3,99,-1,-100]);
     }
 }
