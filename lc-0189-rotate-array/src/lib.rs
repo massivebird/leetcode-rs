6d9b56d5624af43023f90f606274a5ea5a84c9b2
massivebird <33991373+massivebird@users.noreply.github.com>
2025-02-12 12:51:36 -0500

    wip(0189): an idea

--- lc-0189-rotate-array/src/lib.rs
+++ lc-0189-rotate-array/src/lib.rs
@@ -2,7 +2,16 @@ struct Solution;
 
 impl Solution {
     pub fn rotate(nums: &mut Vec<i32>, k: i32) {
-        
+        let mut hand_og_idx: usize = 0;
+        let mut hand: i32 = nums[0];
+
+        // Each value displaces some other value.
+        // Let's move the first one, pick up whatever it
+        // displaces, then move that one.
+
+        for _ in 0..nums.len() {
+
+        }
     }
 }
 
