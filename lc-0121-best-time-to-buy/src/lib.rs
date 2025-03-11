20190a48373e0d4b165919ca5b1e71144ca67537
massivebird <33991373+massivebird@users.noreply.github.com>
2025-03-11 11:31:26 -0400

    init: 0121

---
+++ lc-0121-best-time-to-buy/src/lib.rs
@@ -0,0 +1,17 @@
+struct Solution;
+
+impl Solution {
+    pub fn max_profit(prices: Vec<i32>) -> i32 {
+        todo!();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_1() {
+        assert_eq!(Solution::max_profit(vec![7, 1, 5, 3, 6, 4]), 5);
+    }
+}
