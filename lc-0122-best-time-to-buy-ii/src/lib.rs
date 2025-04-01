bf37d08e76a6276ad28fdfa813eb1b4f48224424
massivebird <33991373+massivebird@users.noreply.github.com>
2025-04-01 10:44:48 -0400

    wip(0122): memory limit exceeded

--- lc-0122-best-time-to-buy-ii/src/lib.rs
+++ lc-0122-best-time-to-buy-ii/src/lib.rs
@@ -1,14 +1,36 @@
 struct Solution;
 
+#[allow(dead_code)]
+#[allow(clippy::needless_pass_by_value)]
 impl Solution {
     pub fn max_profit(prices: Vec<i32>) -> i32 {
-        // I think we find the best profit first, then find next best profits
-        // excluding the timeframe of the first. Repeat until there is no more possible profit.
-        //
-        // But then what if there's a profit of 50 to be gained, but there's also two disjoint
-        // profits of 49 and 49 that overlap with the 50? This strat won't get us the highest
-        // score. Is the question THAT advanced? Maybe
-        todo!()
+        let mut grid: Vec<Vec<i32>> = vec![vec![0; prices.len() + 1]; prices.len() + 1];
+
+        for (i, bought) in prices.iter().enumerate() {
+            for (j, sold) in prices.iter().enumerate() {
+                if j < i {
+                    grid[i + 1][j + 1] = grid[i][j + 1];
+                    continue;
+                }
+
+                // Best profit up to buying on the current day.
+                let base = grid[i][i];
+
+                grid[i + 1][j + 1] = i32::max(sold - bought + base, grid[i][j + 1]);
+            }
+        }
+
+        grid[prices.len()][prices.len()]
+    }
+}
+
+fn my_dbg<T: std::fmt::Display>(v: &Vec<Vec<T>>) {
+    for sv in v {
+        for e in sv {
+            print!("{e:3}");
+        }
+
+        println!();
     }
 }
 
@@ -19,6 +41,24 @@ mod tests {
     #[test]
     fn case_1() {
         let prices = vec![7, 1, 5, 3, 6, 4];
+        assert_eq!(Solution::max_profit(prices), 7);
+    }
+
+    #[test]
+    fn case_2() {
+        let prices = vec![1, 2, 3, 4, 5];
+        assert_eq!(Solution::max_profit(prices), 4);
+    }
+
+    #[test]
+    fn case_3() {
+        let prices = vec![7, 6, 4, 3, 1];
+        assert_eq!(Solution::max_profit(prices), 0);
+    }
+
+    #[test]
+    fn case_4() {
+        let prices = vec![6, 1, 3, 2, 4, 7];
         assert_eq!(Solution::max_profit(prices), 7);
     }
 }
