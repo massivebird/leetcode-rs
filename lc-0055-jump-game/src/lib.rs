935ada73b590604694f29382752e5486824fe6cd
massivebird <33991373+massivebird@users.noreply.github.com>
2025-04-06 04:41:30 -0400

    changed(0055): optimizations

--- lc-0055-jump-game/src/lib.rs
+++ lc-0055-jump-game/src/lib.rs
@@ -7,21 +7,37 @@ impl Solution {
     //
     // Is it possible to jump to the final element in `nums`?
     pub fn can_jump(nums: Vec<i32>) -> bool {
-        // `navigable[x] = 1` means that index `x` is navigable from some
-        // other index.
-        let mut navigable = vec![0; nums.len()];
-        // Start at the first index.
-        navigable[0] = 1;
+        if nums.len() <= 1 {
+            return true;
+        }
 
-        for (idx, max_jump) in nums.into_iter().enumerate() {
-            for jdx in (idx + 1)..=(idx + max_jump as usize) {
-                if let Some(v) = navigable.get_mut(jdx) {
-                    *v = 1;
-                }
+        // Invariant: ALL indices must be navigable in order to reach the end.
+        //
+        // If there is some unreachable index anywhere, then we cannot jump
+        // to it or any other index thereafter, including the end.
+        //
+        // The contrapositive of the above is if we can reach the end, then
+        // all indices are reachable.
+
+        let mut farthest_navigable_idx = 0;
+
+        for (i, dist) in nums.iter().enumerate().take(nums.len() - 1) {
+            // Check if this index is unreachable.
+            if farthest_navigable_idx < i {
+                return false;
+            }
+
+            farthest_navigable_idx = usize::max(farthest_navigable_idx, i + *dist as usize);
+
+            // Check if final index (or beyond) is reachable.
+            if farthest_navigable_idx >= nums.len() - 1 {
+                return true;
             }
         }
 
-        navigable.iter().all(|v| v == &1)
+        // All positions have been exhausted, but the final index is still
+        // unreachable.
+        false
     }
 }
 
@@ -52,5 +68,10 @@ mod tests {
     #[test]
     fn case_4() {
         assert!(Solution::can_jump(vec![2, 0, 0]));
+    }
+
+    #[test]
+    fn case_5() {
+        assert!(!Solution::can_jump(vec![0, 2, 3]));
     }
 }
