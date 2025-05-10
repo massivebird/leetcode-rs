37f54f812d4fe69569c8012c9b52206ba4cb11cb
massivebird <33991373+massivebird@users.noreply.github.com>
2025-05-10 18:05:08 -0400

    feat(0274): complete

--- lc-0274-h-index/src/lib.rs
+++ lc-0274-h-index/src/lib.rs
@@ -3,7 +3,24 @@ struct Solution {}
 #[allow(dead_code, clippy::needless_pass_by_value)]
 impl Solution {
     pub fn h_index(citations: Vec<i32>) -> i32 {
-        todo!()
+        let total_citations = citations.len();
+
+        let mut t = vec![0; total_citations + 1];
+
+        let mut candidate_index = 0;
+        let mut candidate_count = 0;
+
+        for c in citations {
+            for i in (1..=c as usize).take(usize::min(c as usize, total_citations)) {
+                t[i] += 1;
+                if t[i] >= i && i > candidate_index {
+                    candidate_index = i;
+                    candidate_count = t[i];
+                }
+            }
+        }
+
+        candidate_index as i32
     }
 }
 
