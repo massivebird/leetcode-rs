8b4c657d23731ba39388409a92d35af3dc31c50e
massivebird <33991373+massivebird@users.noreply.github.com>
2025-05-11 05:05:04 -0400

    feat(0238): complete

--- lc-0238-product-except-self/src/lib.rs
+++ lc-0238-product-except-self/src/lib.rs
@@ -3,7 +3,34 @@ struct Solution {}
 #[allow(dead_code, clippy::needless_pass_by_value)]
 impl Solution {
     pub fn product_except_self(nums: Vec<i32>) -> Vec<i32> {
-        todo!()
+        let mut all_product = 1;
+        let mut all_product_nonzero = 1;
+        let mut num_zeroes = 0;
+
+        for &num in &nums {
+            all_product *= num;
+
+            if num == 0 {
+                num_zeroes += 1;
+            } else {
+                all_product_nonzero *= num;
+            }
+        }
+
+        let mut ans: Vec<i32> = vec![0; nums.len()];
+
+        for (idx, &num) in nums.iter().enumerate() {
+            if num == 0 {
+                if num_zeroes == 1 {
+                    ans[idx] = all_product_nonzero;
+                }
+                continue;
+            }
+
+            ans[idx] = all_product / num;
+        }
+
+        ans
     }
 }
 
@@ -17,5 +44,18 @@ mod tests {
             Solution::product_except_self(vec![1, 2, 3, 4]),
             vec![24, 12, 8, 6]
         );
+    }
+
+    #[test]
+    fn case_1() {
+        assert_eq!(
+            Solution::product_except_self(vec![-1, 1, 0, -3, 3]),
+            vec![0, 0, 9, 0, 0]
+        );
+    }
+
+    #[test]
+    fn case_2() {
+        assert_eq!(Solution::product_except_self(vec![0, 4, 0]), vec![0, 0, 0]);
     }
 }
