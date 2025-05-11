20fa88001c1b786dfabe4288ce334587a79e5770
massivebird <33991373+massivebird@users.noreply.github.com>
2025-05-11 04:03:37 -0400

    init(0380): initial commit

---
+++ lc-0380-insert-delete-getrandom-o1/src/lib.rs
@@ -0,0 +1,42 @@
+struct RandomizedSet {}
+
+/**
+ * `&self` means the method takes an immutable reference.
+ * If you need a mutable reference, change it to `&mut self` instead.
+ */
+impl RandomizedSet {
+    fn new() -> Self {
+        todo!();
+    }
+
+    fn insert(&self, val: i32) -> bool {
+        todo!();
+    }
+
+    fn remove(&self, val: i32) -> bool {
+        todo!();
+    }
+
+    fn get_random(&self) -> i32 {
+        todo!();
+    }
+}
+
+// Your RandomizedSet object will be instantiated and called as such:
+// let obj = RandomizedSet::new();
+// let ret_1: bool = obj.insert(val);
+// let ret_2: bool = obj.remove(val);
+// let ret_3: i32 = obj.get_random();
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn it_works() {
+//         let obj: RandomizedSet = RandomizedSet::new();
+//         let ret_1: bool = obj.insert(val);
+//         let ret_2: bool = obj.remove(val);
+//         let ret_3: i32 = obj.get_random();
+//     }
+// }
