1233a1a73989f5a631aba49d4940ccbe2af29c65
massivebird <33991373+massivebird@users.noreply.github.com>
2025-05-20 19:51:56 -0400

    feat(0014): complete

---
+++ lc-0014-longest-common-prefix/src/lib.rs
@@ -0,0 +1,53 @@
+struct Solution;
+
+#[allow(unused, clippy::needless_pass_by_value)]
+impl Solution {
+    pub fn longest_common_prefix(strs: Vec<String>) -> String {
+        let mut buf = String::new();
+
+        // You gave me an empty vector? Are you for serious?
+        let Some(first_word) = strs.first() else {
+            return buf;
+        };
+
+        for (i, c) in first_word.char_indices() {
+            if strs
+                .iter()
+                .all(|w| w.chars().nth(i).is_some_and(|o| c == o))
+            {
+                buf.push(c);
+            } else {
+                break;
+            }
+        }
+
+        buf
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_0() {
+        let strs = vec![
+            "flower".to_string(),
+            "flow".to_string(),
+            "flight".to_string(),
+        ];
+
+        assert_eq!(Solution::longest_common_prefix(strs), "fl".to_string());
+    }
+
+    #[test]
+    fn case_1() {
+        let strs = vec![
+            "racecar".to_string(),
+            "dog".to_string(),
+            "clint".to_string(),
+        ];
+
+        assert_eq!(Solution::longest_common_prefix(strs), String::new());
+    }
+}
