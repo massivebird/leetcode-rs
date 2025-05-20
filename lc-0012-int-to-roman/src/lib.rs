5144609a038c4ea17bbf890d85ea3d5aeb4d480b
massivebird <33991373+massivebird@users.noreply.github.com>
2025-05-20 19:37:06 -0400

    changed(0012): optimized, readable

--- lc-0012-int-to-roman/src/lib.rs
+++ lc-0012-int-to-roman/src/lib.rs
@@ -2,48 +2,31 @@ struct Solution {}
 
 #[allow(unused)]
 impl Solution {
-    pub fn int_to_roman(num: i32) -> String {
-        let mut n = num;
-
-        let mut_contains = |n: &mut i32, d: i32| {
-            if *n / d >= 1 {
-                *n -= d;
-                true
-            } else {
-                false
-            }
-        };
+    pub fn int_to_roman(mut num: i32) -> String {
+        let numerals = [
+            ("M", 1000),
+            ("CM", 900),
+            ("D", 500),
+            ("CD", 400),
+            ("C", 100),
+            ("XC", 90),
+            ("L", 50),
+            ("XL", 40),
+            ("X", 10),
+            ("IX", 9),
+            ("V", 5),
+            ("IV", 4),
+            ("I", 1),
+        ];
 
         let mut buf = String::new();
 
-        while n >= 1 {
-            if mut_contains(&mut n, 1000) {
-                buf.push('M');
-            } else if mut_contains(&mut n, 900) {
-                buf.push_str("CM");
-            } else if mut_contains(&mut n, 500) {
-                buf.push('D');
-            } else if mut_contains(&mut n, 400) {
-                buf.push_str("CD");
-            } else if mut_contains(&mut n, 100) {
-                buf.push('C');
-            } else if mut_contains(&mut n, 90) {
-                buf.push_str("XC");
-            } else if mut_contains(&mut n, 50) {
-                buf.push('L');
-            } else if mut_contains(&mut n, 40) {
-                buf.push_str("XL");
-            } else if mut_contains(&mut n, 10) {
-                buf.push('X');
-            } else if mut_contains(&mut n, 9) {
-                buf.push_str("IX");
-            } else if mut_contains(&mut n, 5) {
-                buf.push('V');
-            } else if mut_contains(&mut n, 4) {
-                buf.push_str("IV");
-            } else if mut_contains(&mut n, 1) {
-                buf.push('I');
-            }
+        while num >= 1 {
+            let (symbol, val) = numerals.iter().find(|(_, val)| num / *val >= 1).unwrap();
+
+            num -= *val;
+
+            buf.push_str(symbol);
         }
 
         buf
