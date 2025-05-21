83bd8bd6c706a6c8f45b7c57e6f7cdd5893d4872
massivebird <33991373+massivebird@users.noreply.github.com>
2025-05-20 23:44:56 -0400

    added(0068): mostly comments

--- lc-0068-text-justification/src/lib.rs
+++ lc-0068-text-justification/src/lib.rs
@@ -1,64 +1,75 @@
+//! Given an array of strings words and a width maxWidth,
+//! format the text such that each line has exactly maxWidth
+//! characters and is fully (left and right) justified EXCEPT
+//! that the last line is left-aligned.
+
 struct Solution;
 
 #[allow(unused, clippy::needless_pass_by_value)]
 impl Solution {
     pub fn full_justify(words: Vec<String>, max_width: i32) -> Vec<String> {
+        // Will contain the words present in each line.
         let mut lines: Vec<Vec<String>> = vec![vec![]];
 
-        let acc_len = |v: &Vec<String>| v.iter().fold(0, |acc, s| acc + s.len());
+        // Calculates the cumulative length of all strings in a vector.
+        let acc_len = |v: &[String]| v.iter().fold(0, |acc, s| acc + s.len());
 
+        // Place all words onto their output lines.
         for word in words {
             let line_width = acc_len(lines.last().unwrap());
 
             let num_words = lines.last().unwrap().len();
             let num_spaces = usize::try_from(max_width).unwrap() - line_width;
 
-            // num_spaces must not exceed (num_words - 1)
-
-            // dbg!(&lines);
-            // dbg!(&num_spaces);
-            if num_spaces > num_words
-                && num_spaces > word.len()
-                && num_spaces - word.len() >= num_words
-                && num_spaces > word.len()
-            {
+            // Test if there are enough spaces to accomodate this word, given
+            // that `num_spaces` must not exceed `num_words - 1`.
+            if num_spaces.saturating_sub(word.len()) >= num_words {
                 lines.last_mut().unwrap().push(word);
             } else {
+                // Not enough spaces remaining on this line.
+                // Start a new line.
                 lines.push(vec![word]);
             }
         }
 
-        let mut ans_buf: Vec<String> = Vec::new();
+        // Will contain the returned solution.
+        let mut ans_vec: Vec<String> = Vec::new();
 
-        for line in lines.iter().take(lines.len() - 1).filter(|v| !v.is_empty()) {
+        // Justify-align each line.
+        // Except for the last line, which is left-aligned.
+        for line in lines.iter().take(lines.len() - 1) {
+            // Will contain this line, justified.
             let mut line_buf = String::new();
 
-            let line_width = acc_len(line);
+            let acc_word_len = acc_len(line);
 
-            let total_spaces = usize::try_from(max_width).unwrap() - line_width;
+            let total_spaces = usize::try_from(max_width).unwrap() - acc_word_len;
 
             let num_words = line.len();
 
             if num_words <= 1 {
-                ans_buf.push(format!("{}{}", line[0], " ".repeat(total_spaces)));
+                ans_vec.push(format!("{}{}", line[0], " ".repeat(total_spaces)));
                 continue;
             }
 
             let spaces_per_word = total_spaces / (num_words - 1);
 
-            let additional_spaces = total_spaces % (num_words - 1);
+            // Allocate extra spaces from left to right.
+            let added_spaces_idx = total_spaces % (num_words - 1);
 
             for (idx, word) in line.iter().enumerate() {
                 line_buf.push_str(word);
                 line_buf.push_str(&" ".repeat(spaces_per_word));
 
-                if additional_spaces > 0 && idx < additional_spaces {
+                if added_spaces_idx > 0 && idx < added_spaces_idx {
                     line_buf.push(' ');
                 }
             }
 
-            ans_buf.push(line_buf.trim_end().to_owned());
+            ans_vec.push(line_buf.trim_end().to_owned());
         }
+
+        // Left-justify the last line.
 
         let last_line = {
             let binding = lines
@@ -72,12 +83,12 @@ impl Solution {
             binding.trim_end().to_owned()
         };
 
-        ans_buf.push(format!(
+        ans_vec.push(format!(
             "{last_line}{}",
             " ".repeat(usize::try_from(max_width).unwrap() - last_line.len())
         ));
 
-        ans_buf
+        ans_vec
     }
 }
 
