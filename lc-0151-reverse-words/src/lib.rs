4f6248c91b1dd52a4ec61a103aa51b3d35d20c18
massivebird <33991373+massivebird@users.noreply.github.com>
2025-05-20 20:07:41 -0400

    feat(0151): complete

---
+++ lc-0151-reverse-words/src/lib.rs
@@ -0,0 +1,37 @@
+struct Solution;
+
+#[allow(unused, clippy::needless_pass_by_value)]
+impl Solution {
+    pub fn reverse_words(s: String) -> String {
+        s.split_whitespace()
+            .rev()
+            .fold(String::new(), |acc, s| acc + " " + s)[1..] // Exclude leading space
+            .to_string()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_0() {
+        assert_eq!(
+            Solution::reverse_words("the sky is blue".to_string()),
+            "blue is sky the".to_string()
+        );
+    }
+
+    #[test]
+    fn case_1() {
+        assert_eq!(
+            Solution::reverse_words("  hello world  ".to_string()),
+            "world hello".to_string()
+        );
+    }
+
+    #[test]
+    fn case_2() {
+        assert_eq!(Solution::reverse_words("t ".to_string()), "t".to_string());
+    }
+}
