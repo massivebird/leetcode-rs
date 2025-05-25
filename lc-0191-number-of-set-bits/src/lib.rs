87f673523a49acf2e8fb64ae2712a5c4b8d83922
massivebird <33991373+massivebird@users.noreply.github.com>
2025-05-25 01:14:24 -0400

    feat(0191): complete

---
+++ lc-0191-number-of-set-bits/src/lib.rs
@@ -0,0 +1,23 @@
+struct Solution;
+
+#[allow(unused)]
+impl Solution {
+    pub fn hamming_weight(n: i32) -> i32 {
+        i32::try_from(format!("{n:b}").chars().filter(|c| *c == '1').count()).unwrap()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_0() {
+        assert_eq!(Solution::hamming_weight(11), 3);
+    }
+
+    #[test]
+    fn case_1() {
+        assert_eq!(Solution::hamming_weight(128), 1);
+    }
+}
