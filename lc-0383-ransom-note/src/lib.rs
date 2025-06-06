724300b21ea52d35fdacf76cc3f81aad982e0e3c
massivebird <33991373+massivebird@users.noreply.github.com>
2025-06-06 15:36:12 -0400

    feat(0383): complete

--- lc-0383-ransom-note/src/lib.rs
+++ lc-0383-ransom-note/src/lib.rs
@@ -1,9 +1,31 @@
+use std::collections::HashMap;
+
 struct Solution;
 
 #[allow(unused, clippy::needless_pass_by_value)]
 impl Solution {
+    // Both inputs consist only of lowercase letters.
     pub fn can_construct(ransom_note: String, magazine: String) -> bool {
-        todo!()
+        let mut freq: HashMap<char, u32> = HashMap::new();
+
+        for c in magazine.chars() {
+            freq.entry(c).and_modify(|v| *v += 1).or_insert(1);
+        }
+
+        for c in ransom_note.chars() {
+            let Some(v) = freq.get_mut(&c) else {
+                // This char isn't in the magazine at all.
+                return false;
+            };
+
+            if *v == 0 {
+                return false;
+            }
+
+            *v -= 1;
+        }
+
+        true
     }
 }
 
@@ -24,6 +46,6 @@ mod tests {
         let ransom_note = "aa".to_string();
         let magazine = "ab".to_string();
 
-        assert!(Solution::can_construct(ransom_note, magazine));
+        assert!(!Solution::can_construct(ransom_note, magazine));
     }
 }
