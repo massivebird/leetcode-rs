a6330ff3ad29827a4afcb4b9c0560e997aaeadd4
massivebird <33991373+massivebird@users.noreply.github.com>
2025-06-07 16:48:06 -0400

    feat(0392): complete

--- lc-0392-is-subsequence/src/lib.rs
+++ lc-0392-is-subsequence/src/lib.rs
@@ -3,7 +3,24 @@ struct Solution;
 #[allow(unused, clippy::needless_pass_by_value)]
 impl Solution {
     pub fn is_subsequence(s: String, t: String) -> bool {
-        todo!()
+        let mut s_chars = s.chars();
+        let mut t_chars = t.chars();
+
+        'outer: loop {
+            let Some(s_next) = s_chars.next() else {
+                break;
+            };
+
+            loop {
+                match t_chars.next() {
+                    Some(c) if c == s_next => continue 'outer,
+                    Some(_) => (),
+                    None => return false,
+                }
+            }
+        }
+
+        true
     }
 }
 
