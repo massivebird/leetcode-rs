6f66d82948a5421989b0912d7f94f2cdb235c429
massivebird <33991373+massivebird@users.noreply.github.com>
2025-06-07 17:08:23 -0400

    feat(0003): complete

--- lc-0003-longest-substr-without-repeats/src/lib.rs
+++ lc-0003-longest-substr-without-repeats/src/lib.rs
@@ -3,14 +3,33 @@ struct Solution;
 #[allow(unused, clippy::needless_pass_by_value)]
 impl Solution {
     pub fn length_of_longest_substring(s: String) -> i32 {
-        let mut idx = 0;
-        let mut ans = 0;
+        let mut l_idx: usize = 0;
+        let mut r_idx: usize = 0;
 
-        while idx < s.len() {
+        let mut high_score: i32 = 0;
+        let mut score: i32 = 0;
 
+        'outer: while r_idx < s.len() {
+            let here = s.as_bytes()[r_idx];
+
+            for i in l_idx..r_idx {
+                if s.as_bytes()[i] == here {
+                    // This window contains a duplicate character.
+                    score -= 1;
+                    l_idx += 1;
+
+                    continue 'outer;
+                }
+            }
+
+            score += 1;
+
+            high_score = score.max(high_score);
+
+            r_idx += 1;
         }
 
-        ans
+        high_score
     }
 }
 
@@ -37,6 +56,14 @@ mod tests {
     #[test]
     fn case_2() {
         let s = "pwwkew".to_string();
+        let ans = 3;
+
+        assert_eq!(Solution::length_of_longest_substring(s), ans);
+    }
+
+    #[test]
+    fn case_3() {
+        let s = "dvdf".to_string();
         let ans = 3;
 
         assert_eq!(Solution::length_of_longest_substring(s), ans);
