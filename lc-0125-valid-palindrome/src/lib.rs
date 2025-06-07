ee962ce896e273cc80c5e8a209fcc6b3c23753d7
massivebird <33991373+massivebird@users.noreply.github.com>
2025-06-07 16:39:07 -0400

    feat(0125): complete

--- lc-0125-valid-palindrome/src/lib.rs
+++ lc-0125-valid-palindrome/src/lib.rs
@@ -3,14 +3,29 @@ struct Solution;
 impl Solution {
     #[allow(unused, clippy::needless_pass_by_value)]
     pub fn is_palindrome(s: String) -> bool {
-        let mut l: usize = 0;
-        let mut r: usize = s.len() - 1;
+        let s: String = s
+            .chars()
+            .filter(|c| c.is_alphanumeric())
+            .map(|c| c.to_ascii_lowercase())
+            .collect();
 
-        while l <= r {
-
+        if s.is_empty() {
+            return true;
         }
 
-        todo!()
+        let mut chars = s.chars();
+
+        loop {
+            let (Some(l_char), Some(r_char)) = (chars.nth(0), chars.nth_back(0)) else {
+                break;
+            };
+
+            if l_char != r_char {
+                return false;
+            }
+        }
+
+        true
     }
 }
 
@@ -37,5 +52,12 @@ mod tests {
         let s = " ".to_owned();
 
         assert!(Solution::is_palindrome(s));
+    }
+
+    #[test]
+    fn case_3() {
+        let s = "ab".to_owned();
+
+        assert!(!Solution::is_palindrome(s));
     }
 }
