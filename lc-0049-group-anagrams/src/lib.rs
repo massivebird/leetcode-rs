2d81065de34bf6315813aa03ecac29fa9573d2a5
massivebird <33991373+massivebird@users.noreply.github.com>
2025-06-07 00:27:16 -0400

    feat(0049): complete

--- lc-0049-group-anagrams/src/lib.rs
+++ lc-0049-group-anagrams/src/lib.rs
@@ -3,7 +3,30 @@ struct Solution;
 #[allow(unused, clippy::needless_pass_by_value)]
 impl Solution {
     pub fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
-        todo!()
+        use std::collections::HashMap;
+
+        let mut anagrams: HashMap<[u32; 26], Vec<String>> = HashMap::new();
+
+        for s in strs {
+            let mut freqs = [0u32; 26];
+
+            for c in s.chars() {
+                freqs[(c as u8 - 97) as usize] += 1;
+            }
+
+            anagrams
+                .entry(freqs)
+                .and_modify(|v| v.push(s.clone()))
+                .or_insert_with(|| vec![s]);
+        }
+
+        let mut res = Vec::new();
+
+        for a in anagrams.into_values() {
+            res.push(a);
+        }
+
+        res
     }
 }
 
