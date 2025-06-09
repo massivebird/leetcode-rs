6f892d327dcce30b0a4ecff9970f2f1b5fcdcb61
massivebird <33991373+massivebird@users.noreply.github.com>
2025-06-09 01:20:38 -0400

    feat(0021): complete

--- lc-0021-merge-sorted-lists/src/lib.rs
+++ lc-0021-merge-sorted-lists/src/lib.rs
@@ -20,17 +20,73 @@ impl Solution {
         list1: Option<Box<ListNode>>,
         list2: Option<Box<ListNode>>,
     ) -> Option<Box<ListNode>> {
-        todo!()
+        // I create the final linked list answer out of a vector containing
+        // the combined, sorted values from both lists.
+        let mut ans_vec: Vec<i32> = Vec::new();
+
+        let mut node1 = list1;
+        let mut node2 = list2;
+
+        while let Some(val1) = node1.as_ref().map(|n| n.val) {
+            while let Some(val2) = node2.as_ref().map(|n| n.val) {
+                // Push to the vector any lesser values from list2.
+                if val2 <= val1 {
+                    ans_vec.push(val2);
+                    node2 = node2.unwrap().next;
+                } else {
+                    break;
+                }
+            }
+
+            ans_vec.push(val1);
+
+            node1 = node1.unwrap().next;
+        }
+
+        // Push any remaining list2 values.
+        while let Some(val2) = node2.as_ref().map(|n| n.val) {
+            ans_vec.push(val2);
+            node2 = node2.unwrap().next;
+        }
+
+        // Now we're ready to build the final linked list!
+        let mut ans: Option<Box<ListNode>> = None;
+
+        for val in ans_vec.into_iter().rev() {
+            ans = Some(Box::new(ListNode { val, next: ans }));
+        }
+
+        ans
     }
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-//     #[test]
-//     fn case_0() {
-//         let result = add(2, 2);
-//         assert_eq!(result, 4);
-//     }
-// }
+    #[test]
+    fn case_0() {
+        let list1 = Some(Box::new(ListNode {
+            val: 1,
+            next: Some(Box::new(ListNode { val: 3, next: None })),
+        }));
+
+        let list2 = Some(Box::new(ListNode {
+            val: 2,
+            next: Some(Box::new(ListNode { val: 4, next: None })),
+        }));
+
+        let ans = Some(Box::new(ListNode {
+            val: 1,
+            next: Some(Box::new(ListNode {
+                val: 2,
+                next: Some(Box::new(ListNode {
+                    val: 3,
+                    next: Some(Box::new(ListNode { val: 4, next: None })),
+                })),
+            })),
+        }));
+
+        assert_eq!(Solution::merge_two_lists(list1, list2), ans);
+    }
+}
