88d3d757c3d6bf0af3c820a5ecc95d535ab120aa
massivebird <33991373+massivebird@users.noreply.github.com>
2025-06-18 23:29:13 -0400

    feat(0066): complete

--- lc-0066-plus-one/src/lib.rs
+++ lc-0066-plus-one/src/lib.rs
@@ -1,9 +1,44 @@
+// The input vector contains the digits of a number.
+// Return the equivalent vector of the input value plus one.
+
 struct Solution;
 
 #[allow(unused, clippy::needless_pass_by_value)]
 impl Solution {
     pub fn plus_one(digits: Vec<i32>) -> Vec<i32> {
-        todo!()
+        let mut digits: Vec<i32> = digits.into_iter().rev().collect();
+
+        let mut idx = 0;
+
+        let mut carry = false;
+
+        loop {
+            let digit = digits.get_mut(idx);
+
+            match digit {
+                Some(&mut 9) => {
+                    *digit.unwrap() = 0;
+                    carry = true;
+                }
+                Some(x) if carry => {
+                    *x += 1;
+                    break;
+                }
+                Some(x) => {
+                    *x += 1;
+                    break;
+                }
+                None if carry => {
+                    digits = [digits, vec![1]].concat();
+                    break;
+                }
+                _ => break,
+            }
+
+            idx += 1;
+        }
+
+        digits.into_iter().rev().collect()
     }
 }
 
