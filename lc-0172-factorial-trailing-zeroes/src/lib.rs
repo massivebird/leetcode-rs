85d2e6caf3f9adc0bcecd3fa56a768df14387a0d
massivebird <33991373+massivebird@users.noreply.github.com>
2025-06-20 23:35:15 -0400

    wip(0172): no idea now

--- lc-0172-factorial-trailing-zeroes/src/lib.rs
+++ lc-0172-factorial-trailing-zeroes/src/lib.rs
@@ -3,7 +3,22 @@ struct Solution;
 #[allow(unused)]
 impl Solution {
     pub fn trailing_zeroes(n: i32) -> i32 {
-        todo!()
+        let fac: i128 = (1i128..=i128::from(n)).product();
+
+        if fac % 10 != 0 {
+            return 0;
+        }
+
+        let mut n = 1;
+
+        let mut ans: i32 = 0;
+
+        while fac % 10i128.pow(n) == 0 {
+            ans = n as i32;
+            n += 1;
+        }
+
+        ans
     }
 }
 
@@ -23,5 +38,26 @@ mod tests {
         let n = 5; // -> 120
 
         assert_eq!(Solution::trailing_zeroes(n), 1);
+    }
+
+    #[test]
+    fn case_2() {
+        let n = 7; // -> 5040
+
+        assert_eq!(Solution::trailing_zeroes(n), 1);
+    }
+
+    #[test]
+    fn case_3() {
+        let n = 13;
+
+        assert_eq!(Solution::trailing_zeroes(n), 2);
+    }
+
+    #[test]
+    fn case_4() {
+        let n = 40;
+
+        assert_eq!(Solution::trailing_zeroes(n), 9);
     }
 }
