a9cc56a4a464df3dfccd97ceb74ffeb22d5b4141
massivebird <33991373+massivebird@users.noreply.github.com>
2025-08-18 02:40:56 -0400

    refactored(0209): cleaning up

--- lc-0209-minimum-size-subarray-sum/src/lib.rs
+++ lc-0209-minimum-size-subarray-sum/src/lib.rs
@@ -4,94 +4,63 @@ struct Solution;
 impl Solution {
     #[allow(clippy::needless_pass_by_value, dead_code)]
     pub fn min_sub_array_len(target: i32, nums: Vec<i32>) -> i32 {
-        if nums.len() == 1 {
-            if nums[0] >= target {
-                return 1;
-            }
-
+        // Handle short-circuit opportunities and single-element input arrays.
+        if nums[0] >= target {
+            return 1;
+        } else if nums.len() == 1 {
             return 0;
         }
 
-        if nums[0] >= target {
-            return 1;
-        }
+        // Sliding window properties.
+        let mut ldx: usize = 0;
+        let mut window_len: usize = 2;
 
-        let mut active_sum = nums[0] + nums[1];
+        // Tracking sum of the values within the sliding window.
+        let mut window_sum: i32 = nums[0] + nums[1];
 
-        let mut best_len = 0;
-
-        let mut ldx = 0;
-        let mut window_len = 2;
-
-        let try_shift_ldx = |ldx: usize| {
-            if ldx == nums.len() - 1 {
-                return Err(());
-            }
-
-            Ok(ldx + 1)
-        };
-
-        let try_shift_window = |ldx: usize, window_len: usize| {
-            if ldx + window_len == nums.len() {
-                return Err(());
-            }
-
-            Ok(window_len + 1)
-        };
+        // The shortest subarray length recorded so far.
+        // `0` indicates no valid subarray.
+        let mut best_len: i32 = 0;
 
         loop {
-            // println!("{ldx} {window_len} {active_sum}");
-            match active_sum.cmp(&target) {
-                std::cmp::Ordering::Less => {
-                    match try_shift_window(ldx, window_len) {
-                        Ok(v) => window_len = v,
-                        Err(()) => return best_len,
-                    }
+            if window_sum < target {
+                // Window sum needs to increase.
+                //
+                // Extend window rightwards to eat up more values.
 
-                    active_sum += nums[ldx + window_len - 1];
+                // Bounds check: is the window already on the edge?
+                if ldx + window_len == nums.len() {
+                    return best_len;
                 }
-                std::cmp::Ordering::Equal => {
-                    if best_len == 0 {
-                        best_len = window_len as i32;
-                    } else {
-                        best_len = i32::min(best_len, i32::try_from(window_len).unwrap());
-                    }
 
-                    // Shift entire window right by one.
+                window_len += 1;
 
-                    // Subtract leftmost value.
-                    active_sum -= nums[ldx];
+                window_sum += nums[ldx + window_len - 1];
 
-                    match try_shift_ldx(ldx) {
-                        Ok(v) => ldx = v,
-                        Err(()) => return best_len,
-                    }
-
-                    // Exit if window is OOB.
-                    if ldx + window_len > nums.len() {
-                        return best_len;
-                    }
-
-                    // Add new rightmost value.
-                    active_sum += nums[ldx + window_len - 1];
-                }
-                std::cmp::Ordering::Greater => {
-                    if best_len == 0 {
-                        best_len = window_len as i32;
-                    } else {
-                        best_len = i32::min(best_len, i32::try_from(window_len).unwrap());
-                    }
-
-                    active_sum -= nums[ldx];
-
-                    match try_shift_ldx(ldx) {
-                        Ok(v) => ldx = v,
-                        Err(()) => return best_len,
-                    }
-
-                    window_len -= 1;
-                }
+                continue;
             }
+
+            // Window sum is sufficiently large.
+
+            if best_len == 0 {
+                best_len = i32::try_from(window_len).unwrap();
+            } else {
+                best_len = i32::min(best_len, i32::try_from(window_len).unwrap());
+            }
+
+            // Since the window sum is sufficiently large, try reducing it.
+            //
+            // Shrink the sliding window by shifting its left bound forward.
+
+            window_sum -= nums[ldx];
+
+            // Bounds check: is the left idx already on the rightmost element?
+            if ldx == nums.len() - 1 {
+                return best_len;
+            }
+
+            ldx += 1;
+            window_len -= 1;
         }
     }
 }
