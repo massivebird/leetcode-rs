d1b203ae69717d115d1295c478d377cc3a6303d3
massivebird <33991373+massivebird@users.noreply.github.com>
2025-08-21 17:25:14 -0400

    feat(0070): complete

--- lc-0070-climbing-stairs/src/lib.rs
+++ lc-0070-climbing-stairs/src/lib.rs
@@ -2,8 +2,32 @@ struct Solution;
 
 #[allow(unused)]
 impl Solution {
+    // One possible conceptualization: you are counting the leaf nodes of the
+    // tree(s) representing all possible navigations through the stairs.
     pub fn climb_stairs(n: i32) -> i32 {
-        todo!()
+        if n <= 3 {
+            return n;
+        }
+
+        let n: usize = usize::try_from(n).unwrap();
+
+        // The value at the nth step corresponds to the number of ways
+        // that step can be reached.
+        //
+        // Base cases:
+        // - One way to reach the first step (don't think about it).
+        // - Two ways to reach the second step, either:
+        //   - Stepped distance of 1 on first step, or
+        //   - Stepped distance of 2 on first step.
+        let mut stairs = vec![0; n];
+        stairs[0] = 1;
+        stairs[1] = 2;
+
+        for i in 2..n {
+            stairs[i] = stairs[i - 1] + stairs[i - 2];
+        }
+
+        stairs[n - 1]
     }
 }
 
