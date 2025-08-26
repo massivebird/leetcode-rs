ad916a0eb47d76192da55b1f6bb355d01cb08d45
massivebird <33991373+massivebird@users.noreply.github.com>
2025-08-26 19:02:00 -0400

    wip(0042): see desc
    [5,0,2,0,2] m_idx @ 3 produces problematic l_idx @ 0, r_idx @ 4.
    Considering a simultaneous side bounds check.

--- lc-0042-trapping-rain-water/src/lib.rs
+++ lc-0042-trapping-rain-water/src/lib.rs
@@ -12,7 +12,12 @@ impl Solution {
         while m_idx < height.len() {
             let mut this = height[m_idx];
 
-            if height.get(m_idx + 1).is_some_and(|next| *next >= this) {
+            if m_idx > 0
+                && height[m_idx - 1] > this
+                && height.get(m_idx + 1).is_some_and(|next| *next > this)
+            {
+                ();
+            } else if height.get(m_idx + 1).is_some_and(|next| *next >= this) {
                 m_idx += 1;
                 continue;
             }
@@ -31,13 +36,13 @@ impl Solution {
             let mut l_idx = m_idx.saturating_sub(1);
             for (idx, l_opt) in height.iter().enumerate().take(m_idx).rev() {
                 if *l_opt > l_height {
-                    l_height = l_height.max(height[l_idx]);
+                    l_height = *l_opt;
                     l_idx = idx;
                 }
             }
 
             // Find trap's right bound.
-            let mut r_idx = usize::max(m_idx, height.len() - 1);
+            let mut r_idx = usize::min(m_idx + 1, height.len() - 1);
             let mut r_height = height[r_idx];
             for (idx, r_opt) in height.iter().enumerate().skip(m_idx) {
                 if *r_opt > r_height {
@@ -45,9 +50,14 @@ impl Solution {
                     r_idx = idx;
                 }
 
-                if *r_opt > l_height {
+                if *r_opt >= l_height {
                     break;
                 }
+            }
+
+            // Reached the void on the right side. No trap here.
+            if r_idx == m_idx {
+                break;
             }
 
             let l_height = height[l_idx];
@@ -107,6 +117,30 @@ mod tests {
             6, 4, 2, 0, 3, 2, 0, 3, 1, 4, 5, 3, 2, 7, 5, 3, 0, 1, 2, 1, 3, 4, 6, 8, 1, 3,
         ];
         let ans = 83;
+
+        assert_eq!(Solution::trap(height), ans);
+    }
+
+    #[test]
+    fn case_4() {
+        let height = vec![4, 3, 3, 9, 3, 0, 9, 2, 8, 3];
+        let ans = 23;
+
+        assert_eq!(Solution::trap(height), ans);
+    }
+
+    #[test]
+    fn case_5() {
+        let height = vec![8, 2, 8, 9, 0, 1, 7, 7, 9];
+        let ans = 27;
+
+        assert_eq!(Solution::trap(height), ans);
+    }
+
+    #[test]
+    fn case_6() {
+        let height = vec![0, 1, 2, 0, 3, 0, 1, 2, 0, 0, 4, 2, 1, 2, 5, 0, 1, 2, 0, 2];
+        let ans = 26;
 
         assert_eq!(Solution::trap(height), ans);
     }
