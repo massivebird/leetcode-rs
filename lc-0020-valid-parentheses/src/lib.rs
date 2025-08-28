e94d05621d823c7a61b5716afa1e512e3db16c37
massivebird <33991373+massivebird@users.noreply.github.com>
2025-08-28 17:59:35 -0400

    feat(0020): complete

--- lc-0020-valid-parentheses/src/lib.rs
+++ lc-0020-valid-parentheses/src/lib.rs
@@ -3,7 +3,28 @@ struct Solution;
 #[allow(unused, clippy::needless_pass_by_value)]
 impl Solution {
     pub fn is_valid(s: String) -> bool {
-        todo!()
+        use std::collections::VecDeque;
+        let mut stack: VecDeque<char> = VecDeque::new();
+
+        for c in s.chars() {
+            if matches!(c, '(' | '{' | '[') {
+                stack.push_back(c);
+                continue;
+            }
+
+            let Some(last) = stack.iter().last() else {
+                return false;
+            };
+
+            match (last, c) {
+                ('(', ')') | ('{', '}') | ('[', ']') => {
+                    stack.pop_back();
+                }
+                _ => return false,
+            }
+        }
+
+        stack.is_empty()
     }
 }
 
