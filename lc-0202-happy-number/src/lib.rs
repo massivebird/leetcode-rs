2098d9c8d078c350db0fd60eeedf7ef9f2d2b190
massivebird <33991373+massivebird@users.noreply.github.com>
2025-10-22 13:35:25 -0400

    feat(0202): complete

--- lc-0202-happy-number/src/lib.rs
+++ lc-0202-happy-number/src/lib.rs
@@ -3,8 +3,29 @@ struct Solution;
 
 impl Solution {
     #[allow(unused)]
-    pub fn is_happy(n: i32) -> bool {
-        todo!();
+    pub fn is_happy(mut n: i32) -> bool {
+        let mut known_values = Vec::new();
+
+        loop {
+            n = n
+                .to_string()
+                .chars()
+                .map(|c| c.to_digit(10).unwrap().pow(2))
+                .sum::<u32>() as i32;
+
+            println!("{n}");
+
+            if n == 1 {
+                return true;
+            }
+
+            if known_values.contains(&n) {
+                println!("break {n}");
+                break false;
+            }
+
+            known_values.push(n);
+        }
     }
 }
 
@@ -20,5 +41,10 @@ mod tests {
     #[test]
     fn case_1() {
         assert!(!Solution::is_happy(2));
+    }
+
+    #[test]
+    fn case_2() {
+        assert!(Solution::is_happy(7));
     }
 }
