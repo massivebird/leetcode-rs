5c2d394987edfd0f1962577d5ab22c9b88820d51
massivebird <33991373+massivebird@users.noreply.github.com>
2025-12-09 18:10:56 -0500

    removed(0104): unnecessary param

--- lc-0104-maximum-depth-binary-tree/src/lib.rs
+++ lc-0104-maximum-depth-binary-tree/src/lib.rs
@@ -24,21 +24,24 @@ struct Solution;
 
 use std::cell::RefCell;
 use std::rc::Rc;
-impl Solution {
-    #![allow(clippy::needless_pass_by_value, dead_code)]
 
+#[allow(clippy::needless_pass_by_value, dead_code)]
+impl Solution {
     pub fn max_depth(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
-        Self::eval_subtree(root.as_ref(), 0)
+        Self::eval_subtree(root.as_ref())
     }
 
-    fn eval_subtree(root: Option<&Rc<RefCell<TreeNode>>>, a: i32) -> i32 {
+    fn eval_subtree(root: Option<&Rc<RefCell<TreeNode>>>) -> i32 {
         let Some(root) = root else {
-            return a;
+            return 0;
         };
 
-        let b = Self::eval_subtree(root.borrow().left.as_ref(), a);
-        let c = Self::eval_subtree(root.borrow().right.as_ref(), a);
+        let left_depth = Self::eval_subtree(root.borrow().left.as_ref());
+        let right_depth = Self::eval_subtree(root.borrow().right.as_ref());
 
-        1 + b.max(c)
+        // I wrote this solution on accident.
+        // The more conventional solution increment the depth within each function call.
+        // This method cascades the depth upward! Or whatever you want to call it.
+        1 + left_depth.max(right_depth)
     }
 }
