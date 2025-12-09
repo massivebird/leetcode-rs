545abb641f583dba404a13e6c06a9c3eef37ef17
massivebird <33991373+massivebird@users.noreply.github.com>
2025-12-09 18:59:46 -0500

    wip: asymmetric trees..

--- lc-0226-invert-binary-tree/src/lib.rs
+++ lc-0226-invert-binary-tree/src/lib.rs
@@ -25,6 +25,29 @@ struct Solution;
 #[allow(clippy::needless_pass_by_value, dead_code)]
 impl Solution {
     pub fn invert_tree(root: Option<Rc<RefCell<TreeNode>>>) -> Option<Rc<RefCell<TreeNode>>> {
-        todo!()
+        let Some(root) = root else {
+            return root;
+        };
+
+        Self::swap_roots(root.borrow().left.as_ref(), root.borrow().right.as_ref());
+
+        Some(root)
+    }
+
+    // TODO:
+    // Issue: Asymmetric trees.
+    // Maybe a swap(root) controller, which calls either:
+    // swap_roots(l, r) or swap_lopsided(root).
+
+    fn swap_roots(l: Option<&Rc<RefCell<TreeNode>>>, r: Option<&Rc<RefCell<TreeNode>>>) {
+        match (l, r) {
+            (Some(l), Some(r)) => {
+                std::mem::swap(&mut l.borrow_mut().val, &mut r.borrow_mut().val);
+
+                Self::swap_roots(l.borrow().left.as_ref(), r.borrow().right.as_ref());
+                Self::swap_roots(l.borrow().right.as_ref(), r.borrow().left.as_ref());
+            }
+            _ => (),
+        }
     }
 }
