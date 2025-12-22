a8b3077141b84356ae93710d54226075d4f7c276
massivebird <33991373+massivebird@users.noreply.github.com>
2025-12-22 17:33:26 -0500

    wip(0739): brute force

--- lc-0739-daily-temperatures/src/lib.rs
+++ lc-0739-daily-temperatures/src/lib.rs
@@ -4,7 +4,20 @@ struct Solution;
 impl Solution {
     #[allow(clippy::needless_pass_by_value)]
     pub fn daily_temperatures(temperatures: Vec<i32>) -> Vec<i32> {
-        todo!()
+        let mut buf: Vec<i32> = vec![0; temperatures.len()];
+
+        'outer: for (i, this) in temperatures.iter().enumerate() {
+            for (j, other) in temperatures.iter().enumerate().skip(i + 1) {
+                if other > this {
+                    buf[i] = i32::try_from(j - i).unwrap();
+                    continue 'outer;
+                }
+
+                buf[i] = 0;
+            }
+        }
+
+        buf
     }
 }
 
