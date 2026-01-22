683a67013425a6407afaf6cd04b3a1fafd1247bf
massivebird <33991373+massivebird@users.noreply.github.com>
2026-01-22 16:57:06 -0500

    init(0200): boilerplate

---
+++ lc-0200-number-of-islands/src/lib.rs
@@ -0,0 +1,37 @@
+struct Solution;
+
+impl Solution {
+    #[allow(dead_code, clippy::needless_pass_by_value)]
+    pub fn num_islands(grid: Vec<Vec<char>>) -> i32 {
+        todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_0() {
+        let grid = vec![
+            vec!['1', '1', '1', '1', '0'],
+            vec!['1', '1', '0', '1', '0'],
+            vec!['1', '1', '0', '0', '0'],
+            vec!['0', '0', '0', '0', '0'],
+        ];
+
+        assert_eq!(Solution::num_islands(grid), 1);
+    }
+
+    #[test]
+    fn case_1() {
+        let grid = vec![
+            vec!['1', '1', '0', '0', '0'],
+            vec!['1', '1', '0', '0', '0'],
+            vec!['0', '0', '1', '0', '0'],
+            vec!['0', '0', '0', '1', '1'],
+        ];
+
+        assert_eq!(Solution::num_islands(grid), 3);
+    }
+}
