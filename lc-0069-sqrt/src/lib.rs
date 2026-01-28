95381b7f727d50c12c1e0f62b8195bbf297af632
massivebird <33991373+massivebird@users.noreply.github.com>
2026-01-28 14:47:30 -0500

    feat(0069): complete

--- lc-0069-sqrt/src/lib.rs
+++ lc-0069-sqrt/src/lib.rs
@@ -5,14 +5,32 @@ struct Solution;
 impl Solution {
     #[allow(dead_code)]
     pub fn my_sqrt(x: i32) -> i32 {
-        let mut last: f32 = 1.0; // Initial guess
-        let mut next: f32 = 0.0;
-
-        for _ in 0..10 {
-            next = 0.5 * (last + (x as f32) / last);
-            last = next;
+        let x: i64 = x.into();
+        if x < 2 {
+            return x as i32;
         }
 
-        return next.floor() as i32;
+        let mut l: i64 = 0;
+        let mut r: i64 = x / 2;
+
+        loop {
+            let mid = l.midpoint(r);
+
+            let test = (mid + 1) * (mid + 1);
+
+            if (mid + 1).pow(2) > x && mid.pow(2) < x {
+                return mid as i32;
+            }
+
+            if test == x {
+                return (mid + 1) as i32;
+            } else if test > x {
+                r = mid;
+            } else if l == mid {
+                l = r;
+            } else {
+                l = mid;
+            }
+        }
     }
 }
