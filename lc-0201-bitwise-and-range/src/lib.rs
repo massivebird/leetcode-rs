f22abe7bcab6f5a8a1cc4cd642cae011e1a2eb3b
massivebird <33991373+massivebird@users.noreply.github.com>
2026-01-27 23:31:34 -0500

    init(0201): boilerplate

---
+++ lc-0201-bitwise-and-range/src/lib.rs
@@ -0,0 +1,19 @@
+struct Solution;
+
+impl Solution {
+    pub fn range_bitwise_and(left: i32, right: i32) -> i32 {
+        todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_0() {
+        let left = 5;
+        let right = 7;
+        assert_eq!(Solution::range_bitwise_and(left, right), 4);
+    }
+}
