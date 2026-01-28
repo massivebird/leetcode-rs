9b64144a13e3dcd93d32d8c19d4ced7c148ea1e5
massivebird <33991373+massivebird@users.noreply.github.com>
2026-01-27 23:35:23 -0500

    feat(0201): complete

--- lc-0201-bitwise-and-range/src/lib.rs
+++ lc-0201-bitwise-and-range/src/lib.rs
@@ -1,8 +1,14 @@
+//! Given two integers `left` and `right` that represent the range `[left, right]`, return the bitwise AND of all numbers in this range, inclusive.
+//!
+//! MB: Apparently the runtime-optimal solution involves some bitwise dark arts.
+//! I'll (totally) do that later.
+
 struct Solution;
 
 impl Solution {
+    #[allow(dead_code)]
     pub fn range_bitwise_and(left: i32, right: i32) -> i32 {
-        todo!()
+        (left..=right).reduce(|acc, n| acc & n).unwrap()
     }
 }
 
