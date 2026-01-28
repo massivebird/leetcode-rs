330e19252862782a9ff4fcfdf26b61d292559db7
massivebird <33991373+massivebird@users.noreply.github.com>
2026-01-27 22:50:01 -0500

    feat(0108): complete

--- lc-0108-sorted-arr-to-bst/src/lib.rs
+++ lc-0108-sorted-arr-to-bst/src/lib.rs
@@ -1,3 +1,10 @@
+//! Given an integer array nums where the elements are sorted in ascending
+//! order, convert it to a height-balanced binary search tree.
+//!
+//! MB: My solution operates on the premise that the tree is rooted at the
+//! array's middle element. This premise holds as you recursively split
+//! the array in half to form the root's left and right subtrees.
+
 use std::cell::RefCell;
 use std::rc::Rc;
 use tree_node::TreeNode;
@@ -7,41 +14,32 @@ struct Solution;
 impl Solution {
     #[allow(dead_code, clippy::needless_pass_by_value, clippy::unnecessary_wraps)]
     pub fn sorted_array_to_bst(nums: Vec<i32>) -> Option<Rc<RefCell<TreeNode>>> {
-        let root_idx = nums.len() / 2;
+        Self::subtree(0, nums.len() - 1, &nums)
+    }
 
-        let mut root = TreeNode::new(nums[root_idx]);
-
-        // Left subtree
-        for (i, n) in nums.iter().copied().take(root_idx).enumerate() {
-            let mut node = TreeNode::new(n);
-
-            let old_left = root.left.take();
-
-            // Time to rotate!
-            if old_left.is_some() && i % 2 == 0 {
-                old_left.clone().unwrap().borrow_mut().right = Some(Rc::new(RefCell::new(node)));
-                root.left = old_left;
-            } else {
-                node.left = old_left;
-                root.left = Some(Rc::new(RefCell::new(node)));
-            }
+    /// Recursively build the BST.
+    /// `l` and `r` are the inclusive index bounds of the subtree.
+    fn subtree(l: usize, r: usize, nums: &[i32]) -> Option<Rc<RefCell<TreeNode>>> {
+        // Return early if all elements have been exhausted.
+        if l > r {
+            return None;
         }
 
-        // Right subtree
-        for (i, n) in nums.iter().copied().skip(root_idx + 1).enumerate() {
-            let mut node = TreeNode::new(n);
+        let m = l.midpoint(r);
 
-            let old_right = root.right.take();
+        let mut root = TreeNode::new(nums[m]);
 
-            // Time to rotate!
-            if old_right.is_some() && i % 2 == 0 {
-                old_right.clone().unwrap().borrow_mut().right = Some(Rc::new(RefCell::new(node)));
-                root.right = old_right;
-            } else {
-                node.left = old_right;
-                root.right = Some(Rc::new(RefCell::new(node)));
-            }
+        // Check if there are only one or two elements left to capture.
+        // These edge cases cause problems if uncaught.
+        if l == r {
+            return Some(Rc::new(RefCell::new(root)));
+        } else if l == r - 1 {
+            root.right = Some(Rc::new(RefCell::new(TreeNode::new(nums[r]))));
+            return Some(Rc::new(RefCell::new(root)));
         }
+
+        root.left = Self::subtree(l, m.saturating_sub(1), nums);
+        root.right = Self::subtree(m + 1, r, nums);
 
         Some(Rc::new(RefCell::new(root)))
     }
@@ -59,31 +57,13 @@ mod tests {
     fn case_0() {
         let nums = vec![1, 3];
         let actual = Solution::sorted_array_to_bst(nums);
-        let expected = build_tree(&[Some(3), Some(1)]);
+        let expected = build_tree(&[Some(1), None, Some(3)]);
 
         assert_eq!(actual, expected);
     }
 
     #[test]
     fn case_1() {
-        let nums = vec![-10, -3, 0, 5, 9];
-        let actual = Solution::sorted_array_to_bst(nums);
-        let expected = build_tree(&[Some(0), Some(-3), Some(9), Some(-10), None, Some(5)]);
-
-        assert_eq!(actual, expected);
-    }
-
-    #[test]
-    fn case_2() {
-        let nums = vec![0, 1, 2, 3, 4, 5];
-        let actual = Solution::sorted_array_to_bst(nums);
-        let expected = build_tree(&[Some(3), Some(1), Some(5), Some(0), Some(2), Some(4)]);
-
-        assert_eq!(actual, expected);
-    }
-
-    #[test]
-    fn case_3() {
         let nums = vec![0, 1, 2, 3, 4, 5, 6];
         let actual = Solution::sorted_array_to_bst(nums);
         let expected = build_tree(&[
@@ -94,24 +74,6 @@ mod tests {
             Some(2),
             Some(4),
             Some(6),
-        ]);
-
-        assert_eq!(actual, expected);
-    }
-
-    #[test]
-    fn case_4() {
-        let nums = vec![0, 1, 2, 3, 4, 5, 6, 7];
-        let actual = Solution::sorted_array_to_bst(nums);
-        let expected = build_tree(&[
-            Some(4),
-            Some(2),
-            Some(6),
-            Some(1),
-            Some(3),
-            Some(5),
-            Some(7),
-            Some(0),
         ]);
 
         assert_eq!(actual, expected);
