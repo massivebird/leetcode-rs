0f6f2faaabb439b22c59c1eda7c2ed265ba67ced
massivebird <33991373+massivebird@users.noreply.github.com>
2026-01-30 13:31:50 -0500

    wip(0139): failing one test

--- lc-0139-word-break/src/lib.rs
+++ lc-0139-word-break/src/lib.rs
@@ -5,6 +5,64 @@ struct Solution;
 impl Solution {
     #[allow(dead_code, clippy::needless_pass_by_value)]
     pub fn word_break(s: String, word_dict: Vec<String>) -> bool {
-        todo!();
+        let mut buf: Vec<(&str, usize)> = Vec::with_capacity(s.len());
+        let mut prev_cmpd = false;
+
+        for (i, c) in s.chars().enumerate() {
+            dbg!(c);
+            // Remove tracked words that don't fit anymore.
+            buf.retain(|(s, i)| s.chars().nth(i + 1).is_some_and(|o| o == c));
+
+            dbg!(&buf);
+            if i > 0 && !prev_cmpd && buf.is_empty() {
+                return false;
+            }
+
+            for (_s, i) in &mut buf {
+                *i += 1;
+            }
+
+            // Remove completed tracked words.
+            let init_len = buf.len();
+            buf.retain(|(s, i)| *i < s.len() - 1);
+            let post_len = buf.len();
+
+            let just_cmpd = init_len != post_len;
+
+            for word in &word_dict {
+                if
+                // Skip words that end in the character that just completed
+                // other words
+                (!prev_cmpd && just_cmpd && word.chars().nth(0).is_some_and(|o| c == o))
+                    // Skip words that don't start with this character
+                    || word.chars().nth(0).is_none_or(|o| c != o)
+                {
+                    continue;
+                }
+
+                buf.push((word, 0));
+            }
+
+            let init_len = buf.len();
+            buf.retain(|(s, i)| *i < s.len() - 1);
+            let post_len = buf.len();
+
+            let just_cmpd = just_cmpd || (init_len != post_len);
+
+            if just_cmpd {
+                prev_cmpd = true;
+
+                // Completed on the last character!
+                if i == s.len() - 1 {
+                    return true;
+                }
+
+                continue;
+            }
+
+            prev_cmpd = false;
+        }
+
+        buf.iter().any(|(s, i)| *i == s.len() - 1)
     }
 }
--- lc-0139-word-break/src/tests.rs
+++ lc-0139-word-break/src/tests.rs
@@ -13,3 +13,62 @@ fn case_0() {
 
     assert!(Solution::word_break(s, word_dict));
 }
+
+#[test]
+fn case_1() {
+    let s = "catsandog".to_string();
+
+    let word_dict = ["cats", "dog", "sand", "and", "cat"]
+        .iter()
+        .map(|&s| String::from(s))
+        .collect::<Vec<String>>();
+
+    assert!(!Solution::word_break(s, word_dict));
+}
+
+#[test]
+fn case_2() {
+    let s = "abcd".to_string();
+
+    let word_dict = ["a", "abc", "b", "cd"]
+        .iter()
+        .map(|&s| String::from(s))
+        .collect::<Vec<String>>();
+
+    assert!(Solution::word_break(s, word_dict));
+}
+
+#[test]
+fn case_3() {
+    let s = "a".to_string();
+
+    let word_dict = std::iter::once(&"b")
+        .map(|&s| String::from(s))
+        .collect::<Vec<String>>();
+
+    assert!(!Solution::word_break(s, word_dict));
+}
+
+#[test]
+fn case_4() {
+    let s = "adcad".to_string();
+
+    let word_dict = ["cbd", "ad"]
+        .iter()
+        .map(|&s| String::from(s))
+        .collect::<Vec<String>>();
+
+    assert!(!Solution::word_break(s, word_dict));
+}
+
+#[test]
+fn case_5() {
+    let s = "leetcode".to_string();
+
+    let word_dict = ["leet", "etcode"]
+        .iter()
+        .map(|&s| String::from(s))
+        .collect::<Vec<String>>();
+
+    assert!(!Solution::word_break(s, word_dict));
+}
