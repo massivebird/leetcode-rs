80a3e5847a5ad5587738badf0aaf38027c29ef04
massivebird <33991373+massivebird@users.noreply.github.com>
2026-03-03 23:51:17 -0500

    feat(0046): complete

--- lc-0046-permutations/src/lib.rs
+++ lc-0046-permutations/src/lib.rs
@@ -1,10 +1,36 @@
+//! Given an array nums of distinct integers, return all the possible permutations.
+//! You can return the answer in any order.
+//!
+//! MB: my reliable backtracking approach!
+
 struct Solution;
 
 #[allow(dead_code)]
 impl Solution {
     #[allow(clippy::needless_pass_by_value)]
     pub fn permute(nums: Vec<i32>) -> Vec<Vec<i32>> {
-        todo!();
+        let mut acc: Vec<Vec<i32>> = Vec::new();
+
+        Self::rec(Vec::new(), &nums, &mut acc);
+
+        acc
+    }
+
+    fn rec(this: Vec<i32>, rem: &[i32], acc: &mut Vec<Vec<i32>>) {
+        // Used up all the available numbers.
+        // Push the completed permutation!
+        if rem.is_empty() {
+            acc.push(this);
+            return;
+        }
+
+        for r in rem {
+            Self::rec(
+                [this.clone(), [*r].into()].concat(),
+                &rem.iter().copied().filter(|v| v != r).collect::<Vec<i32>>(),
+                acc,
+            );
+        }
     }
 }
 
