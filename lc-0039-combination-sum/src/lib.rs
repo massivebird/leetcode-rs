97fc16b46cd6916460a5a53d0dd9566f24132da6
massivebird <33991373+massivebird@users.noreply.github.com>
2026-03-04 01:15:11 -0500

    feat(0039): complete

--- lc-0039-combination-sum/src/lib.rs
+++ lc-0039-combination-sum/src/lib.rs
@@ -11,43 +11,36 @@ impl Solution {
     pub fn combination_sum(candidates: Vec<i32>, target: i32) -> Vec<Vec<i32>> {
         let mut acc: Vec<Vec<i32>> = Vec::new();
 
-        Self::rec(
-            &mut vec![0; candidates.len()],
-            &candidates,
-            target,
-            &mut acc,
-        );
+        Self::rec(&mut Vec::new(), &candidates, target, &mut acc);
 
         acc
     }
 
     fn rec(this: &mut Vec<i32>, candidates: &[i32], target: i32, acc: &mut Vec<Vec<i32>>) {
-        let sum: i32 = this
-            .iter()
-            .zip(candidates)
-            .fold(0, |acc, (val, mul)| acc + val * mul);
+        let sum: i32 = this.iter().sum();
 
         if sum > target {
             return;
         } else if sum == target {
-            let values: Vec<i32> = this
-                .iter()
-                .enumerate()
-                .flat_map(|(i, m)| vec![candidates[i]; *m as usize])
-                .collect();
-
-            if !acc.contains(&values) {
-                acc.push(values);
+            if !acc.contains(this) {
+                acc.push(this.clone());
             }
+
             return;
         }
 
-        for i in 0..candidates.len() {
-            this[i] += 1;
+        // Ignore candidates that precede the one last used.
+        for c in candidates.iter().skip(
+            candidates
+                .iter()
+                .position(|o| this.last().is_some_and(|t| o == t))
+                .unwrap_or_default(),
+        ) {
+            this.push(*c);
 
             Self::rec(this, candidates, target, acc);
 
-            this[i] -= 1;
+            this.pop();
         }
     }
 }
@@ -61,6 +54,23 @@ mod tests {
         let nums = [2, 3, 6, 7];
         let target = 7;
         let ans = [vec![2, 2, 3], vec![7]];
+
+        assert_eq!(Solution::combination_sum(nums.into(), target), ans);
+    }
+
+    #[test]
+    fn case_1() {
+        let nums = [7, 3, 2];
+        let target = 18;
+        let ans = [
+            vec![7, 7, 2, 2],
+            vec![7, 3, 3, 3, 2],
+            vec![7, 3, 2, 2, 2, 2],
+            vec![3, 3, 3, 3, 3, 3],
+            vec![3, 3, 3, 3, 2, 2, 2],
+            vec![3, 3, 2, 2, 2, 2, 2, 2],
+            vec![2, 2, 2, 2, 2, 2, 2, 2, 2],
+        ];
 
         assert_eq!(Solution::combination_sum(nums.into(), target), ans);
     }
