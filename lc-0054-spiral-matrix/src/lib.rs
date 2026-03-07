5a14f7d91b935c80afc1e23b07bb57f1e0b9a134
massivebird <33991373+massivebird@users.noreply.github.com>
2026-03-07 02:20:58 -0500

    wip(0054): not necessarily square

--- lc-0054-spiral-matrix/src/lib.rs
+++ lc-0054-spiral-matrix/src/lib.rs
@@ -5,6 +5,52 @@ struct Solution;
 impl Solution {
     #[allow(dead_code, clippy::needless_pass_by_value)]
     pub fn spiral_order(matrix: Vec<Vec<i32>>) -> Vec<i32> {
-        todo!()
+        if matrix.is_empty() {
+            return Vec::new();
+        }
+
+        let mut i = 0;
+        let mut j = 0;
+
+        let mut turns = 0;
+
+        let capacity = matrix.len() * matrix[0].len();
+        let mut ans: Vec<i32> = Vec::with_capacity(capacity);
+
+        let mut pos = true;
+
+        ans.push(matrix[i][j]);
+
+        let mut side_len = matrix[0].len() - 1;
+
+        loop {
+            if ans.len() == capacity {
+                return ans;
+            }
+
+            dbg!((pos, turns % 2 == 0, side_len));
+            for _ in 0..side_len {
+                match (pos, turns % 2 == 0) {
+                    // x-axis
+                    (true, true) => j += 1,
+                    (false, true) => j -= 1,
+                    // y-axis
+                    (true, false) => i -= 1,
+                    (false, false) => i += 1,
+                }
+
+                ans.push(matrix[i][j]);
+                dbg!(&ans);
+            }
+
+            turns += 1;
+            if turns % 2 != 0 {
+                pos ^= true;
+            }
+            // Alternate axis positive directions every two turns.
+            if turns != 1 && turns % 2 != 0 {
+                side_len -= 1;
+            }
+        }
     }
 }
--- lc-0054-spiral-matrix/src/tests.rs
+++ lc-0054-spiral-matrix/src/tests.rs
@@ -9,3 +9,27 @@ fn case_0() {
 
     assert_eq!(Solution::spiral_order(matrix), answer);
 }
+
+#[test]
+fn case_1() {
+    let matrix = vec![vec![1]];
+    let answer = [1];
+
+    assert_eq!(Solution::spiral_order(matrix), answer);
+}
+
+#[test]
+fn case_2() {
+    let matrix = vec![];
+    let answer = [];
+
+    assert_eq!(Solution::spiral_order(matrix), answer);
+}
+
+#[test]
+fn case_3() {
+    let matrix = vec![vec![1, 2, 3, 4], vec![5, 6, 7, 8], vec![9, 10, 11, 12]];
+    let answer = [1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7];
+
+    assert_eq!(Solution::spiral_order(matrix), answer);
+}
