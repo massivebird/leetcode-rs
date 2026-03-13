fd265fae551ee99abbf831a930f59b26624004c3
massivebird <33991373+massivebird@users.noreply.github.com>
2026-03-13 09:42:12 -0400

    feat(0230): complete

--- lc-0230-kth-smallest-in-bst/src/lib.rs
+++ lc-0230-kth-smallest-in-bst/src/lib.rs
@@ -1,6 +1,20 @@
-use std::rc::Rc;
-use std::cell::RefCell;
+//! Given the root of a binary search tree, and an integer k,
+//! return the kth smallest value (1-indexed) of all the values
+//! of the nodes in the tree.
+//!
+//! MB: The most diabolical recursive solution I've ever conceived.
+//! Fueled by an air-fried burrito and a banana.
+//!
+//! I don't collect the tree's values in a vec and then index the vec
+//! I SKIPPED that approach because I'm a FREAK OF NATURE I'm a RUNTIME ADDICT
+//!
+//! Node values are `i32` but are restricted to `0 <= x`,
+//! which means the negative space is free to use.
 
+use std::cell::RefCell;
+use std::rc::Rc;
+
+// local workspace subcrate
 use tree_node::TreeNode;
 
 mod tests;
@@ -10,6 +24,42 @@ struct Solution;
 impl Solution {
     #[allow(dead_code, clippy::needless_pass_by_value)]
     pub fn kth_smallest(root: Option<Rc<RefCell<TreeNode>>>, k: i32) -> i32 {
-        todo!()
+        Self::rec(root.as_ref(), k)
+    }
+
+    /// For some node `n`, returns the following values:
+    /// -1: value of `n`'s parent is the solution.
+    /// x < 0: solution is `x` nodes away.
+    /// x >= 0: the solution is x.
+    fn rec(root: Option<&Rc<RefCell<TreeNode>>>, k: i32) -> i32 {
+        let Some(root) = root.map(|r| r.borrow()) else {
+            return -k;
+        };
+
+        let mut recv = Self::rec(root.left.as_ref(), k);
+
+        // dbg!((root.val, l));
+
+        // Solution already found in left subtree. Propagate it.
+        if !recv.is_negative() {
+            return recv;
+        } else if recv == -1 {
+            return root.val;
+        }
+
+        if let Some(r) = root.right.as_ref() {
+            let r = Self::rec(Some(r), -recv - 1);
+            // dbg!((root.val, r));
+
+            // Solution found in right subtree. Propagate it.
+            if r.is_positive() {
+                return r;
+            }
+
+            recv = (r - 1).max(recv);
+        }
+
+        // No solution yet.
+        recv + 1
     }
 }
--- lc-0230-kth-smallest-in-bst/src/tests.rs
+++ lc-0230-kth-smallest-in-bst/src/tests.rs
@@ -6,9 +6,9 @@ use super::Solution;
 
 #[test]
 fn case_0() {
-    let root = build_tree(&[Some(3), Some(1), Some(4), None, Some(2)]);
+    let root = build_tree(&[Some(4), Some(2), Some(5), None, Some(3)]);
     let k = 1;
-    let answer = 1;
+    let answer = 2;
 
     assert_eq!(Solution::kth_smallest(root, k), answer);
 }
@@ -16,17 +16,35 @@ fn case_0() {
 #[test]
 fn case_1() {
     let root = build_tree(&[
-        Some(5),
-        Some(3),
         Some(6),
-        Some(2),
         Some(4),
+        Some(7),
+        Some(3),
+        Some(5),
         None,
         None,
-        Some(1),
+        Some(2),
     ]);
     let k = 3;
-    let answer = 3;
+    let answer = 4;
+
+    assert_eq!(Solution::kth_smallest(root, k), answer);
+}
+
+#[test]
+fn case_2() {
+    let root = build_tree(&[Some(5), None, Some(6)]);
+    let k = 2;
+    let answer = 6;
+
+    assert_eq!(Solution::kth_smallest(root, k), answer);
+}
+
+#[test]
+fn case_3() {
+    let root = build_tree(&[Some(4), Some(2), Some(5), None, Some(3)]);
+    let k = 3;
+    let answer = 4;
 
     assert_eq!(Solution::kth_smallest(root, k), answer);
 }
