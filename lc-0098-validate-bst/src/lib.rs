5e6dfa39f84230e755a2468f546332b0a68d8987
massivebird <33991373+massivebird@users.noreply.github.com>
2026-03-14 06:26:12 -0400

    feat(0098): complete

--- lc-0098-validate-bst/src/lib.rs
+++ lc-0098-validate-bst/src/lib.rs
@@ -1,5 +1,13 @@
-use std::rc::Rc;
+//! Given the root of a binary tree, determine if it is a valid BST.
+//!
+//! MB: inefficient but functional solution! For every node, I evaluate
+//! the min and max values in its subtrees.
+//!
+//! TODO: add min and max args to rec fn and propagate constraints
+//! _downwards_ into subtrees.
+
 use std::cell::RefCell;
+use std::rc::Rc;
 
 use tree_node::TreeNode;
 
@@ -10,6 +18,52 @@ struct Solution;
 impl Solution {
     #[allow(dead_code, clippy::needless_pass_by_value)]
     pub fn is_valid_bst(root: Option<Rc<RefCell<TreeNode>>>) -> bool {
-        todo!()
+        let Some(root) = root.as_ref() else {
+            return false;
+        };
+
+        Self::rec(root)
+    }
+
+    fn rec(root: &Rc<RefCell<TreeNode>>) -> bool {
+        let val = root.borrow().val;
+
+        let mut valid = true;
+
+        if let Some(l) = root.borrow().left.as_ref() {
+            // dbg!((val, Self::eval(l, true)));
+            if Self::eval(l, true) >= val || Self::eval(l, false) >= val {
+                return false;
+            }
+
+            valid &= Self::rec(l);
+        }
+
+        if let Some(r) = root.borrow().right.as_ref() {
+            // dbg!((val, Self::eval(r, false)));
+            if Self::eval(r, false) <= val || Self::eval(r, true) <= val {
+                return false;
+            }
+
+            valid &= Self::rec(r);
+        }
+
+        valid
+    }
+
+    fn eval(root: &Rc<RefCell<TreeNode>>, min: bool) -> i32 {
+        let mut p = root.borrow().val;
+
+        let f = if min { i32::min } else { i32::max };
+
+        if let Some(l) = root.borrow().left.as_ref() {
+            p = f(p, Self::eval(l, min));
+        }
+
+        if let Some(r) = root.borrow().right.as_ref() {
+            p = f(p, Self::eval(r, min));
+        }
+
+        p
     }
 }
--- lc-0098-validate-bst/src/tests.rs
+++ lc-0098-validate-bst/src/tests.rs
@@ -9,3 +9,25 @@ fn case_0() {
     let root = build_tree(&[Some(2), Some(1), Some(3)]);
     assert!(Solution::is_valid_bst(root));
 }
+
+#[test]
+fn case_1() {
+    let root = build_tree(&[Some(5), Some(1), Some(4), None, None, Some(3), Some(6)]);
+    assert!(!Solution::is_valid_bst(root));
+}
+
+#[test]
+fn case_2() {
+    let root = build_tree(&[
+        Some(32),
+        Some(26),
+        Some(47),
+        Some(19),
+        None,
+        None,
+        Some(56),
+        None,
+        Some(27),
+    ]);
+    assert!(!Solution::is_valid_bst(root));
+}
